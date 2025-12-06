72a1091aabd2cf81ec8af6492654fc49c2d980b2
Tamanna-salma <tamannaummey26@gmail.com>
2025-12-07 01:34:38 +0600

    users api added

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//moddleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.nlnjuiz.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+app.get('/', (req, res) => {
+  res.send('bookcourier server is running')
+
+})
+
+async function run() {
+  try {
+    
+    await client.connect();
+    const db=client.db('book_courier_db');
+    const userscollection=db.collection('users');
+   
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email }
+      const existinguser = await userscollection.findOne(query);
+      if (existinguser) {
+        res.send({ massage: 'user already exits.' })
+      }
+      else {
+        const result = await userscollection.insertOne(newUser);
+        res.send(result);
+      }
+
+    })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`bookcourier server is running on port ${port}`);
+})  
